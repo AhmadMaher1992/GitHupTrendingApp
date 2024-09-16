159db1b2f78f8f4b89d053d524c421621f464481
Ahmed Essa <ahmed.e@genie9.com>
2024-09-16 18:18:44 +0300

    add custom segment to handle switching between different times

--- GitHupTrendingApp/Views/TrendingListVC.swift
+++ GitHupTrendingApp/Views/TrendingListVC.swift
@@ -13,6 +13,32 @@ class TrendingListVC: UIViewController {
     // MARK: - properties
     //------------------------------------------
     
+    private let viewModel = TrendingListViewModel()
+    private var refreshControl: UIRefreshControl!
+    private let searchController = UISearchController(searchResultsController: nil)
+    var timeframe: Timeframe = .day // Set a default value
+    
+    
+    //------------------------------------------
+    // MARK: -IBOutlets
+    //------------------------------------------
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: CustomSegment! {
+        
+        didSet
+        {
+            
+            segmentedControl.backgroundColor = AppColors.disabled_Btn_Color
+            segmentedControl.thumbColor = AppColors.nav_Bar_COLOR
+            segmentedControl.font = UIFont(name: "Avenir-Black", size: 12)
+            segmentedControl.selectedLabelColor = .white
+            segmentedControl.unselectedLabelColor = .black
+            segmentedControl.padding = 0
+            segmentedControl.selectedIndex = 0
+            segmentedControl.addTarget(self, action: #selector(timeframeChanged(_:)), for: .valueChanged)
+        }
+    }
     
     
     
@@ -39,9 +65,24 @@ class TrendingListVC: UIViewController {
     
     
     
-    
+    @objc
+    func timeframeChanged(_ sender: AnyObject?)
+    {
+        switch segmentedControl.selectedIndex
+        {
+        case 0 : timeframe = .day
+        case 1: timeframe = .week
+        case 2 : timeframe = .month
+        default:
+            break
+        }
+        
+    }
     
     
     
 }
 
+enum Timeframe {
+    case day, week, month
+}
