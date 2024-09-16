2b2dc13c7e8ca9d7200e1f94f11484e77a03e8a6
Ahmed Essa <ahmed.e@genie9.com>
2024-09-16 20:55:25 +0300

    make progress hud  to show while api request

--- GitHupTrendingApp/Extensions/JGProgressHUD+Extension.swift
+++ GitHupTrendingApp/Extensions/JGProgressHUD+Extension.swift
@@ -5,4 +5,21 @@
 //  Created by Ahmad Eisa on 16/09/2024.
 //
 
-import Foundation
+import UIKit
+import JGProgressHUD
+extension UIViewController{
+
+    static let hud = JGProgressHUD(style: .dark)
+
+    func showHUD(_ show: Bool , withText text: String? = "Loading..."){
+        view.endEditing(true)
+
+        UIViewController.hud.textLabel.text = text
+        if show{
+            UIViewController.hud.show(in: view)
+        }else{
+            UIViewController.hud.dismiss()
+        }
+
+    }
+}
