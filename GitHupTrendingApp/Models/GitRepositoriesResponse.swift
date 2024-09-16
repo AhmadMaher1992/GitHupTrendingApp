b57760cf10e0076373d354ae54d50c9bc141577b
Ahmed Essa <ahmed.e@genie9.com>
2024-09-16 17:52:35 +0300

    create new model to handle api response

--- GitHupTrendingApp/Models/GitRepositoriesResponse.swift
+++ GitHupTrendingApp/Models/GitRepositoriesResponse.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct GithupRepo: Codable {
+    let id: Int?
+    let name: String?
+    let owner: Owner?
+    let description: String?
+    let stargazers_count: Int?
+    let language: String?
+    let forks: Int?
+    let created_at: String?
+    let html_url: String?
+
+   
+}
+struct Owner: Codable {
+    let login: String?
+    let avatar_url: String?
+}
+struct GitRepositoriesResponse: Codable {
+    let items: [GithupRepo]?
+}
+
