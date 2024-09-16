d6fb051ee3e77cdb22af8bcd440efd5b1a9e1fcd
Ahmed Essa <ahmed.e@genie9.com>
2024-09-16 17:47:29 +0300

    make design to GitRepoCell

--- GitHupTrendingApp/Cells/RepositoryCell/RepositoryCell.swift
+++ GitHupTrendingApp/Cells/RepositoryCell/RepositoryCell.swift
@@ -7,17 +7,72 @@
 
 import UIKit
 
-class RepositoryCell: UITableViewCell {
+protocol RepositoryCellDelegate: AnyObject {
+    func handleGitRepoFavorited(gitRepod: GithupRepo)
+    
+}
 
+
+class RepositoryCell: UITableViewCell {
+    
+    
+    //------------------------------------------
+    // MARK: -IBOutlets
+    //------------------------------------------
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var favouriteRepoBtn: UIButton!
+    
+    
+    //------------------------------------------
+    // MARK: - awakeFromNib
+    //------------------------------------------
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+    
+    
+    
+    //------------------------------------------
+    // MARK: -IBActions
+    //------------------------------------------
+    
+    
+    @IBAction func favouriteRepoBtnAction(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    //------------------------------------------
+    // MARK: - configure
+    //------------------------------------------
+    
+    func configure(with repository: GithupRepo) {
+        
+        titleLabel.text = repository.owner?.login ?? ""
+        nameLabel.text = repository.name ?? ""
+        descriptionLabel.text = repository.description ?? "No description available"
+        
+        
+        if let stargazers_count = repository.stargazers_count {
+            starsLabel.text = "⭐ \(stargazers_count)"
+        }
+        
+        if let urlString = repository.owner?.avatar_url {
+            self.avatarImageView.setImage(with: urlString)
+        }
     }
     
 }
