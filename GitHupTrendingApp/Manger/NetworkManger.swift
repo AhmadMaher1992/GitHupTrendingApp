c211e0b4c6cf3ab2b734d73df2ec7e051d44dc60
Ahmed Essa <ahmed.e@genie9.com>
2024-09-16 07:40:46 +0300

    make app architecture

---
+++ GitHupTrendingApp/Manger/NetworkManger.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManger.swift
+//  GitHupTrendingApp
+//
+//  Created by Ahmad Eisa on 16/09/2024.
+//
+
+import UIKit
+
+//-------------------------------------------------
+// MARK: - NetworkManger --> handle Api Services
+//-------------------------------------------------
+class NetworkManger {
+    
+    
+    
+}
