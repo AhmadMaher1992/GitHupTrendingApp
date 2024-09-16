0d8092a42274d765a337c47fc082720d80855350
Ahmed Essa <ahmed.e@genie9.com>
2024-09-16 17:50:02 +0300

    add constants of images , viewcontrollers , storyboards , ..etc

--- GitHupTrendingApp/Utils/Constants.swift
+++ GitHupTrendingApp/Utils/Constants.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+
+import UIKit
+
+struct Storyboards {
+    static let Main = "Main"
+}
+
+
+struct ViewControllerID {
+  
+}
+
+
+struct AppImages {
+    static let filledStar = "filled_star"
+    static let emptyStar = "empty_star"
+    static let placeholder = "person.circle.fill"
+}
+
+struct AppColors {
+    static let blue = #colorLiteral(red: 0.2274509804, green: 0.2666666667, blue: 0.3607843137, alpha: 1)
+    static let red = #colorLiteral(red: 0.8352941176, green: 0.3921568627, blue: 0.3137254902, alpha: 1)
+    static let ofWhite = #colorLiteral(red: 0.9529411765, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+}
+
+
+struct Identifiers {
+    static let RepositoryCell = "RepositoryCell"
+}
