b1935ae4f365b741b935ec2aa815d06760df1aed
Ahmed Essa <ahmed.e@genie9.com>
2024-09-17 04:42:26 +0300

    add this extension to avoid out of range error to tableView

--- GitHupTrendingApp/Extensions/Array+Extension.swift
+++ GitHupTrendingApp/Extensions/Array+Extension.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+extension Array {
+  /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+  subscript(safe index: Int) -> Element? {
+    return indices.contains(index) ? self[index] : nil  /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+  }
+}
+
